7bcb2b5b58233266aca4b5b38634d4f8b592fef5
Fazeel <156305680+fazeel03@users.noreply.github.com>
2024-07-10 23:18:44 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,20 @@
+//strings , numbers , boolean , null,undefined
+
+const name = 'Fazeel';
+const age = 28;
+const rating = 8.9;
+const iscool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof iscool)
+
+console.log(typeof age)
+
+
+console.log(typeof y)
+
+
+console.log(typeof name)
+
